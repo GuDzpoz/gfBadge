e299209dd93c686ef94733cd0fc25da16b2acd40
GuDzpoz <GuDzpoz@live.com>
2021-07-02 15:27:07 +0800

    Added license and copyright info

--- src/components/License.vue
+++ src/components/License.vue
@@ -1,18 +1,70 @@
 <template>
+<div>
   <h3>License</h3>
   <p>
-    This project is licensed under <a href="https://github.com/GuDzpoz/gfBadge/blob/main/LICENSE">GNU Affero General Public License v3.0</a>.
+    The code part of this project is licensed under <a href="https://github.com/GuDzpoz/gfBadge/blob/main/LICENSE">GNU Affero General Public License v3.0</a>.
     <br>
     The source code is available at <a href="https://github.com/GuDzpoz/gfBadge">https://github.com/GuDzpoz/gfBadge</a>.
   </p>
-  <h3>Libraries</h3>
+  <h3>Media files</h3>
   <p>
-    <!-- TODO -->
+    This project utilizes icons from <a href="https://github.com/Templarian/MaterialDesign">Material Design Icons</a>.
+    <br>
+    SUNBORN Network Technology Co. holds the copyright on all other media files.
+    <br>
   </p>
+  <blockquote style="border-left: 0.2rem solid grey; padding-left: 1em">
+    上海暗冬网络科技有限公司 版权所有
+    <br>
+    SUNBORN Network Technology Co., Ltd. All Rights Reserved.
+  </blockquote>
+  <h3>Libraries</h3>
+  <w-accordion :items="[{}]">
+    <template #item-title>Libraries ({{ Object.keys(licenses).length }})</template>
+    <template #item-content>
+      <p>
+        <ul>
+          <li v-for="library in Object.keys(licenses)" :key="library">
+            <b>{{ library }}</b><br>
+            <div>
+              License:
+              <a href="{{ licenses[library].licenseUrl }}">
+                {{ licenses[library].licenses }}
+              </a>
+              <br>
+              Repo:
+              <a href="{{ licenses[library].repository }}">
+                {{ licenses[library].repository }}
+              </a>
+              <br>
+            </div>
+        </li>
+      </ul>
+    </p>
+  </template>
+</w-accordion>
+</div>
 </template>
 
 <script>
+import licenses from '../assets/licenses.json'
+
 export default {
-  name: 'License'
+  name: 'License',
+  data () {
+    return {
+      licenses,
+    }
+  },
 }
 </script>
+
+<style scoped>
+* {
+  padding-top: 0.5em;
+}
+
+div, li {
+  margin-left: 1em;
+}
+</style>
